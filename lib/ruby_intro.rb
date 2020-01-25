c7578c1bc03ce1207f0250730af605f09bb6b487
saziaup <sazigithub@gmail.com>
2020-01-25 15:09:18 +0000

    part 2 and 3 work

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -12,7 +12,7 @@ def max_2_sum(array)
     y = 0
     sortedarray = array.sort
     newarray = sortedarray.reverse 
-  y = newarray[0]+ newarray[1] 
+    y = newarray[0]+ newarray[1] 
   return y
 end
 
@@ -27,16 +27,40 @@ def hello(name)
   return "Hello #{name}"
 end
 
-def starts_with_consonant? s
- 
+def starts_with_consonant?(s)
+s =~ /\A[^aeiou]/i ? "True" : "False"
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
-end
+def binary_multiple_of_4?(s)
+   y = 0
+   array = s.chars
+   array.map(&:to_i)
+    array.each do |x| 
+      if x == 1
+      y =+ 2^x
+      else 
+      y =+ 0
+      end 
+    end
+  y % 4 == 0 ? "true": "false"
+  end 
+
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    
+    attr_accessor :isbn
+    attr_accessor :price
+
+    def initialize (isbn, price)
+   
+    @isbn = isbn
+    @price = price
+    end
+
+    def price_as_string
+ # return "$#{price}.00"
+    end 
+
 end
