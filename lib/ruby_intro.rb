e44d01ccc59473b0146b1bc79a36bbf6f34b010a
saziaup <sazigithub@gmail.com>
2020-01-31 12:29:15 +0000

    solving one and two

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,66 +1,67 @@
-# When done, submit this entire file to the autograder.
+# When done, submit this entire file to the autograder.
 
-# Part 1
+# Part 1
 
-def sum(array)  
-    y = 0
-    array.each { |x| y+= x } 
-    return y
+def sum array
+	if array.empty?
+		return 0
+	else
+		return array.inject(:+)
+	end
 end
 
-def max_2_sum(array)
-    y = 0
-    sortedarray = array.sort
-    newarray = sortedarray.reverse 
-    y = newarray[0]+ newarray[1] 
-  return y
+def max_2_sum arr
+	if arr.empty?
+		return 0
+	else
+		return arr.sort.last(2).inject(:+)
+	end
 end
 
-def sum_to_n?(array, n)
-  array.combination(2).any? {|x, y| return false if (x + y)==n }
-  false
+def sum_to_n? arr, n
+	arr.combination(2).any? { |x, y| x + y == n }
 end
 
-# Part 2
+# Part 2
 
-def hello(name)
-  return "Hello #{name}"
+def hello(name)
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant?(s)
-s =~ /\A[^aeiou]/i ? "True" : "False"
+def starts_with_consonant? s
+  /^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
-def binary_multiple_of_4?(s)
-   y = 0
-   array = s.chars
-   array.map(&:to_i)
-    array.each do |x| 
-      if x == 1
-      y =+ 2^x
-      else 
-      y =+ 0
-      end 
-    end
-  y % 4 == 0 ? "true": "false"
-  end 
+def binary_multiple_of_4? s
+    y = 0
+    array = s.chars
+    array.map(&:to_i)
+    array.each do |x| 
+        if x == 1
+            y =+ 2^x
+        else 
+            y =+ 0
+        end 
 
-
-# Part 3
-class BookInStock
-    
-    attr_accessor :isbn
-    attr_accessor :price
-
-    def initialize (isbn, price)
-   
-    @isbn = isbn 
-    @price = price
-    end
-
-    def price_as_string
- # return "$#{price}.00"
-    end 
-
+    if y % 4 == 0 
+        puts "True"
+    else 
+        puts "False"
+        end 
 end
 
+# Part 3
+
+class BookInStock
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%.2f" % @price
+  end
+end
